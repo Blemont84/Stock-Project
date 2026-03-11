3c863a6e25b976b151e63e6bf2140d4833bc0d2e
A Student <student@nowhere.edu>
2026-03-11 21:52:42 +0000

    Mar 11, 2026, 2:52 PM

--- stockFunctions.cpp
+++ stockFunctions.cpp
@@ -87,14 +87,14 @@ double maxDrawdown(const std::vector<double>& values, int startDay, int endDay)
   double maxDrop = 0.0;
 
   for (int i = startDay; i <= endDay; i++) {
-
     if (values[i] > peak) {
       peak = values[i];
     }
     double drop = values[i] - peak;
-    
+
     if (drop < maxDrop) {
       maxDrop = drop;
     }
   }
+  return maxDrop;
 }
